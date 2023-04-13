8482ee5147251e99ae14ed716902a831023c7976
Pompey21 <marko.mekjavic@gmail.com>
2023-04-13 21:05:45 +0200

    successfully send message as string and converting it into int!

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,30 +1,39 @@
-use core::num;
-#[allow(dead_code)]
-use std::io::{Read, Write, Result};
-use std::net::{TcpStream, SocketAddr};
+use std::io::{Read, Write};
+use std::net::{TcpStream};
 
-fn send_number(addr: SocketAddr, number: i32) -> Result<()> {
-    let mut stream = TcpStream::connect(addr)?;
-    let bytes = number.to_be_bytes();
-    stream.write_all(&bytes)?;
-    Ok(())
-}
 
+// fn main() {
+//     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+//     let message = b"Hello, server!";
+//     let mut num_message: i64 = 42;
+//     let num_message_str = num_message.to_string();
+
+//     // send message to server
+//     stream.write_all(&num_message.to_be_bytes()).unwrap();
+//     stream.write_all(&num_message_str.as_bytes()).unwrap();
+
+//     // receive response from server
+//     let mut buffer = [0u8; 8];
+//     stream.read(&mut buffer).unwrap();
+//     let received_num_message = i64::from_be_bytes(buffer);
+//     println!("Received: {}\n",received_num_message);
+// }
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-    let message = b"Hello, server!";
-    let num_message: i64 = 42;
+    let num_message: i32 = 42;
+    let num_message_str = num_message.to_string();
 
     // send message to server
-    // stream.write_all(message).unwrap();
-    stream.write_all(&num_message.to_be_bytes()).unwrap();
-    // send_number(stream.peer_addr().unwrap(), num_message);
+    // stream.write_all(&num_message.to_be_bytes()).unwrap();
+    stream.write_all(&num_message_str.as_bytes()).unwrap();
 
     // receive response from server
-    let mut buffer = [0u8; 8];
+    let mut buffer = [0u8; 4];
     stream.read(&mut buffer).unwrap();
-    let received_num_message = i64::from_be_bytes(buffer);
-    println!("Received: {}\n",received_num_message);
-}
 
+    let received_num_message_str = String::from_utf8_lossy(&buffer).into_owned();
+
+    // let received_num_message = i32::from_be_bytes(buffer);
+    println!("Received: {}\n",received_num_message_str);
+}
