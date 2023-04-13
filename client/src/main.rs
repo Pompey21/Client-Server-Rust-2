b6bd24ab3af82a68b4a6cc89e5c4e9e7ca8d302d
Pompey21 <marko.mekjavic@gmail.com>
2023-04-13 17:56:22 +0200

    managed to send a number over but need to figure out how to accept it as a number!

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,24 @@
-use std::io::{Read, Write};
-use std::net::TcpStream;
+use core::num;
+#[allow(dead_code)]
+use std::io::{Read, Write, Result};
+use std::net::{TcpStream, SocketAddr};
+
+fn send_number(addr: SocketAddr, number: i32) -> Result<()> {
+    let mut stream = TcpStream::connect(addr)?;
+    let bytes = number.to_be_bytes();
+    stream.write_all(&bytes)?;
+    Ok(())
+}
+
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
     let message = b"Hello, server!";
+    let num_message = 123456789;
 
     // send message to server
     stream.write_all(message).unwrap();
+    send_number(stream.peer_addr().unwrap(), num_message);
 
     // receive response from server
     let mut buffer = [0; 512];
