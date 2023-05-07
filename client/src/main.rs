6f54c93ca2e20b58bf6bfa5e99041c2f51dbe14f
Pompey21 <marko.mekjavic@gmail.com>
2023-05-07 23:11:12 +0200

    issue closed, separated the handling of the sending requests on the user side

--- client/src/main.rs
+++ client/src/main.rs
@@ -14,48 +14,44 @@ use offer::Offer;
 
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-
-    // create a user object
-    let user_x = User::new("Marko".to_string(), "123".to_string(), 100, 100, false);
-    let my_serialized_user_x = bincode::serialize(&user_x).unwrap();
-    // println!("{my_serialized_user_x:?}");
-    let my_deserialized_user_x = bincode::deserialize::<User>(&my_serialized_user_x).unwrap();
-    // println!("{my_deserialized_user_x:?}");
-
-    // create an offer object
-    let offer_x = Offer::new(100, 10, 100);
 
 // Sending the first request
+    // create a user object
+    let user_x = User::new("Marko".to_string(), "123".to_string(), 100, 100, false);
+    // create an offer object
+    let offer_x = Offer::new(100, 10, 100);
     // create a request object
-    let request_x = Request::new("POST".to_string(), "USER".to_string(), "".to_string(), user_x, offer_x);
-    let my_serialized_request_x = bincode::serialize(&request_x).unwrap();
-    stream.write_all(&my_serialized_request_x).unwrap();
+    let request_x = Request::new("POST".to_string(), "USER".to_string(), "".to_string(), user_x.clone(), offer_x);
+
+    // send request to server
+    send_request(request_x);
+
+
+
+// Sending the second request
+    // create a request object
+    let offer_y: Offer = Offer::new(100, 10, 100);
+    let request_y = Request::new("GET".to_string(), "USER".to_string(), "".to_string(), user_x.clone(), offer_y);
+
+    // send request to server
+    send_request(request_y);
+
+    loop {
+        thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
+
+// ======================================
+// Send Over Requests
+// ======================================
+fn send_request(request: Request) {
+    // send message to server
+    let mut stream: TcpStream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    let my_serialized_request = bincode::serialize(&request).unwrap();
+    stream.write_all(&my_serialized_request).unwrap();
 
     // receive response from server
     const SIZE_OF_REQUEST: usize = std::mem::size_of::<Request>();
     let mut buffer = [0; SIZE_OF_REQUEST];
     stream.read(&mut buffer).unwrap();
-
-    let returned_message: Request = bincode::deserialize::<Request>(&buffer).unwrap();
-    println!("Received: {:?}\n", returned_message);
-
-// Sending the second request
-    // create a request object
-    let offer_y: Offer = Offer::new(100, 10, 100);
-    let request_y = Request::new("GET".to_string(), "USER".to_string(), "".to_string(), my_deserialized_user_x, offer_y);
-    let my_serialized_request_y = bincode::serialize(&request_y).unwrap();
-    stream.write_all(&my_serialized_request_y).unwrap();
-
-    // receive response from server
-    // const SIZE_OF_REQUEST: usize = std::mem::size_of::<Request>();
-    let mut buffer = [0; SIZE_OF_REQUEST];
-    stream.read(&mut buffer).unwrap();
-
-    let returned_message: Request = bincode::deserialize::<Request>(&buffer).unwrap();
-    println!("Received: {:?}\n", returned_message);
-
-    loop {
-        thread::sleep(std::time::Duration::from_secs(1));
-    }
 }
